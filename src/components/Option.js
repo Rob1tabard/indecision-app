92bb3354453c4d5ace4d4f90569137de59c85b83
Robin <rpctab@gmail.com>
2019-01-24 14:11:39 +0200

    importing files

---
+++ src/components/Option.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Option = (props) => (
+  <div className="option">
+    <p className="option__text">{props.count}. {props.optionText}</p>
+    <button 
+    className="button button--remove"
+    onClick={(e)=>{
+      props.handleDeleteOption(props.optionText)
+    }}
+    >
+    remove</button>
+  </div>
+)
+
+
+  export default Option
